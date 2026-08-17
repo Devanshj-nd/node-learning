2a04db7180131f785465782689d39ee5c7cd8976
Devanshj-nd <devansh.joshi.c@netradyne.com>
2026-08-17 11:52:43 +0530

    Updated Process runtime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 // Adding comment
+// import { cwd, version, platform, argv } from 'node:process';
+
 const message = 'Hello Node.js';
 console.log(message);
+console.log("argv", process.argv0);
+console.log("cwd", process.cwd());
+console.log("version", process.version);
+console.log("platform", process.platform);
+
+
